6e0e5dd74ff84655fe324f3409c35fb7a734c598
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-04-18 02:09:04 +0200

    genxs()
    + inne przyklady

--- hopfieldasync.py
+++ hopfieldasync.py
@@ -1,4 +1,6 @@
 import numpy as np
+import itertools
+
 
 # klasa dynamiczna bo pewnie w projekt cz3 bedzie nauczanie tej sieci
 # bo to w cz2 to mozna za pomocą funkcji zrobić
@@ -38,18 +40,36 @@ def exec(x,w,b,g):
     else:
       print(str(xIn) + " => " + str(xOut))
 
+# generuje wszystkie kombinacje wektora o n długości
+# i dwóch mozliwych wartościach
+def genxs(n, v1, v2):
+  return np.array([list(i) for i in itertools.product([v1, v2], repeat=n)])
 
 print("---przykład z notatek---")
 w = np.array([[0,1],[1,0]])
-x = np.array([[0,0],[0,1],[1,0],[1,1]])
+x = genxs(2,0,1)
 b = np.array([0,0])
+g = lambda x: 1 if x > 0 else 0
+exec(x,w,b,g)
+
+print("---przykład z ZadRozwiazaniaHopfield.pdf str 9---")
+w = np.array([[-1,3/4],[3/4,0]])
+x = genxs(2,0,1)
+b = np.array([0,0])
+g = lambda x: 1 if x >= 0 else 0
+exec(x,w,b,g)
+
+print("---przykład z ZadRozwiazaniaHopfield.pdf str 13---")
+w = np.array([[0,1,-1],[1,0,1],[-1,1,0]])
+x = genxs(3,0,1)
+b = np.array([0,0,0])
 g = lambda x: 1 if x > 0 else 0
 exec(x,w,b,g)
 
 print("---projekt---")
 c = 2/3
 w = np.array([[0,-c,c],[-c,0,-c],[c,-c,0]])
-x = np.array([[-1,-1,-1],[-1,-1,1],[-1,1,-1],[-1,1,1],[1,-1,-1],[1,-1,1],[1,1,-1],[1,1,1]])
+x = genxs(3,-1,1)
 b = np.array([0,0,0])
 g = lambda x: 1 if x > 0 else -1
 exec(x,w,b,g)
