7e5b2fe5085008658d0f7949aa3e3d8beae85b76
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-05-02 13:17:13 +0200

    testing files

---
+++ testPerceptron.py
@@ -0,0 +1,23 @@
+import numpy as np
+from Perceptron import Perceptron
+
+w0 = np.array([1,1,1])
+r = 1
+f = lambda x: 1 if x > 0 else 0
+dk = [0,0,1,0]
+xk = np.array([[1,0,0],[1,0,1],[1,1,0],[1,1,1]])
+
+# testowe z ćwiczeń
+# w0 = np.array([0.5,0,1])
+# r = 1
+# f = lambda x: 1 if x > 0 else 0
+# dk = [0,0,1,0]
+# xk = np.array([[1,0,0],[1,1,0],[1,1,1],[1,0,1]])
+
+p0 = Perceptron(f,w0,r)
+p0.learn(xk,dk)
+
+print("----algorytm zgrupowany----")
+
+p1 = Perceptron(f,w0,r)
+p1.learn(xk,dk,grouped=True)
